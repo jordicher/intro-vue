df4f7d12cb3565ea105b0291003c95a904e41705
jordicher <jordi.armengol@sunmedia.tv>
2022-05-28 12:46:40 +0200

    lifecycle hooks

--- options-platzi/src/App.vue
+++ options-platzi/src/App.vue
@@ -1,26 +1,3 @@
-<script>
-import { defineAsyncComponent } from "vue";
-import Menu from "./components/Menu.vue";
-
-const HelloWorld = defineAsyncComponent(() =>
-  import("./components/HelloWorld.vue")
-);
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    Menu,
-  },
-  data() {
-    return {
-      helloWorld: HelloWorld,
-      show: false,
-    };
-  },
-};
-</script>
-
 <template>
   <header>
     <div class="wrapper">
@@ -38,10 +15,89 @@ export default {
     CSS property of the element. -->
   </nav>
 
+  <Modal />
+
   <!--   <component :is="helloWorld"> </component>
  -->
   <!-- se renderiza a partir de un componente dinamico, solo lo podemos hacer en componentes que ya esten diponibles -->
 </template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import Menu from "./components/Menu.vue";
+import Modal from "./components/Modal.vue";
+
+const HelloWorld = defineAsyncComponent(() =>
+  import("./components/HelloWorld.vue")
+);
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Menu,
+    Modal,
+  },
+  data() {
+    return {
+      helloWorld: HelloWorld,
+      show: false,
+    };
+  },
+
+  //beforeCreate Se llama cuando la instancia se inicializa, después de que las props se resuelven y antes de procesar opciones como data() o computed
+
+  beforeCreate() {
+    //código a ejecutar
+    console.log("beforeCreate", this.$data, this.$el);
+  },
+  /* 
+      created se llama cuando la instancia ha terminado de procesar todas las opciones relacionados con el estado, como lo son: datos reactivos, propiedades computadas, métodos y observadores, pero aún no comienza la fase de montado y la función $el aún no esta disponible.
+   */
+  created() {
+    //lógica a ejecutar
+    console.log("created", this.$data, this.$el);
+  },
+
+  /*  beforeMount Se llama cuando el componente ha sido montado, pero no existen nodos en el DOM aún, pero están a punto de ser ejecutado el render el DOM por primera vez, no se llama su usamos renderizado del lado de servidor. */
+
+  beforeMount() {
+    //lógica  a ejecutar
+    console.log("beforeMount", this.$data, this.$el);
+  },
+
+  /* mounted Se llama cuando el componente ha sido montado, se considera montado cuando: todos sus componentes hijos síncronos han sido montados, no se incluyen los asíncronos o los que estén en modo suspendido < Suspense > , cuando su propio árbol del DOM ha sido creado e insertado en su contenedor padre.Tampoco es llamado durante renderizado del lado del servidor */
+
+  mounted() {
+    //logica a ejecutar
+    console.log("mounted", this.$data, this.$el);
+  },
+
+  /*  beforeUpdate Se llama justo cuando el componente esta por actualizar su árbol del DOM, debido a un cambio reactivo.Este hook se puede usar para acceder al DOM justo cuando Vue esta por hacer un cambio.
+   */
+  beforeUpdate() {
+    //logica a ejecutar
+  },
+
+  /*updated Se llama justo cuando el componente ha sido actualizado por un cambio en su estado, En caso del padre su método se llamará cuando los métodos de sus hijos hayan terminado.Es importante tener cuidado si no somos precavidos podemos provocar ciclos de renderizado infinito. */
+
+  updated() {
+    //logica a ejecutar
+  },
+
+  /*beforeUnmount Se llama justo cuando la instancia esta por salir del DOM, cuando usamos este hook la instancia que esta por salir aun es funcional */
+
+  beforeUnmount() {
+    //logica a ejecutar
+  },
+
+  /*unmounted se llama cuando el componente es desmontado, un componente se considera desmontado cuando: todos su hijos han sido desmontados y todos sus efectos reactivos se han detenido, este hook se utiliza cuando queremos limpiar de manera manual algunos efectos como cuentas regresivas, conexiones con el server o eventos del DOM. */
+
+  unmounted() {
+    //logica a ejecutar
+  },
+};
+</script>
 
 <style>
 @import "./assets/base.css";
---
+++ options-platzi/src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <teleport to="body">
+      <!-- to => selector de css valido -->
+      <div v-show="show">
+        <h1>Titulo</h1>
+        <p>Lorem Ipsum</p>
+
+        <button @click="toggle">Cerrar</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
