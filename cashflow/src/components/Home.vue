15c78973da3c5aeeb1e556694d0cb80f934064a5
jordicher <jordi.armengol@sunmedia.tv>
2022-06-02 22:13:43 +0200

    layout structure

--- cashflow/src/components/Home.vue
+++ cashflow/src/components/Home.vue
@@ -3,10 +3,18 @@
     <template #header>
       <Header> </Header>
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </Layout>
 </template>
 
 <script setup>
 import Layout from "./Layout.vue";
 import Header from "./Header.vue";
+import Resume from "./resume/Resume.vue";
+import Movements from "./Movements.vue";
 </script>
